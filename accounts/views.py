b4175f7361de37d646e66f80a6859b178d3861a3
Amber <amber.jng@kidsnote.com>
2023-10-14 18:48:08 +0900

    [wanted]계정 뷰셋 추가

--- accounts/views.py
+++ accounts/views.py
@@ -10,8 +10,7 @@ User = get_user_model()
 
 
 @extend_schema(request=UserSerializer, responses=UserSerializer)
-class UserViewSet(mixins.CreateModelMixin,
-                  mixins.RetrieveModelMixin,
+class UserViewSet(mixins.RetrieveModelMixin,
                   mixins.UpdateModelMixin,
                   mixins.DestroyModelMixin,
                   GenericViewSet):
